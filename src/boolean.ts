69c6987f84c43f571f331887a528ed3f4fbd525a
= <lindellcarternyc@gmail.com>
2023-02-12 18:26:07 -0500

    feat: implements boolean schema

--- src/boolean.ts
+++ src/boolean.ts
@@ -1,6 +1,6 @@
 import { CGSchema, CGSchemaOptions } from './CGSchema'
 
-export const boolean = (options: CGSchemaOptions): CGSchema<boolean> => {
+export const boolean = (options: CGSchemaOptions = {}): CGSchema<boolean> => {
   const schema = new CGSchema<boolean>(
     (value) => typeof value === 'boolean',
     options
