412525d0d4e75770152f61097e7f663d5e6b5dcf
= <lindellcarternyc@gmail.com>
2023-02-13 17:26:16 -0500

    chore(number): adds and exports class version

--- src/number.ts
+++ src/number.ts
@@ -1,9 +1,11 @@
 import { CGSchema, CGSchemaOptions } from './CGSchema'
 
-export const number = (options: CGSchemaOptions = {}): CGSchema<number> => {
-  const schema = new CGSchema<number>(
-    (value) => typeof value === 'number',
-    options
-  )
+export class CGNumber extends CGSchema<number> {
+  constructor(options: CGSchemaOptions = {}) {
+    super((value) => typeof value === 'number', options)
+  }
+}
+export const number = (options: CGSchemaOptions = {}): CGNumber => {
+  const schema = new CGNumber(options)
   return schema
 }
