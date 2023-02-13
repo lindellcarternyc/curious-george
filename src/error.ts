74d4b04f1b6d1ea5b29078dd0f30689c83f29a58
= <lindellcarternyc@gmail.com>
2023-02-13 13:51:35 -0500

    feat(error): adds optional path field

--- src/error.ts
+++ src/error.ts
@@ -3,14 +3,17 @@ type CGErrorCode = '__required__' | '__invalid_type__' | '__literal__'
 interface CGErrorArgs {
   code: CGErrorCode
   message: string
+  path?: string
 }
 
 export class CGError {
   readonly code: CGErrorCode
   readonly message: string
+  readonly path?: string
 
-  constructor({ code, message }: CGErrorArgs) {
+  constructor({ code, message, path }: CGErrorArgs) {
     this.code = code
     this.message = message
+    this.path = path
   }
 }
